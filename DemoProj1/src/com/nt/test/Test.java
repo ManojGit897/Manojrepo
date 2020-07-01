3a9c3d82350725729dec1690e4b6a481136d3345
dev1 <dev1@gmail.com>
2020-07-01 13:06:30 +0530

    manoj

---
+++ DemoProj1/src/com/nt/test/Test.java
@@ -0,0 +1,16 @@
+package com.nt.test;
+
+import com.nt.beans.Utility;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		
+		
+		Utility U=new Utility();
+		System.out.println(U.getByDate());
+	}
+
+}
